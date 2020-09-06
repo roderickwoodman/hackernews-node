eac565d97a8e4219fc199cd8174c0c0939c44fe9
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-09-05 22:00:59 -0700

    Adds a link to the database

--- src/script.js
+++ src/script.js
@@ -6,6 +6,12 @@ const prisma = new PrismaClient()
 
 //3
 async function main() {
+  const newLink = await prisma.link.create({
+      data: {
+          description: 'Fullstack tutorial for GraphQL',
+          url: 'www.howtographql.com',
+      },
+  })
   const allLinks = await prisma.link.findMany()
   console.log(allLinks)
 }
