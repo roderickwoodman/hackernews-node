c8f67eb9f394848cb17bbe86a00401cae57a55fa
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-09-05 23:05:39 -0700

    Adds an instance of Prisma Client to GraphQL server context

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { PrismaClient } = require('@prisma/client')
 
 let links = [{
   id: 'link-0',
@@ -28,8 +29,12 @@ const resolvers = {
 }
 
 // 3: the allowed APIs and their handlers
+const prisma = new PrismaClient()
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: {
+    prisma,
+  }
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
